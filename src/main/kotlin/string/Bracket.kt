e51d4131c66b7d5f8487b027ea7079863bee1c0e
minho <my_alsgh@naver.com>
2022-08-18 17:30:45 +0900

    [Feat] Bracket Complete

---
+++ src/main/kotlin/string/Bracket.kt
@@ -0,0 +1,25 @@
+package string
+
+
+
+fun main(){
+    repeat(readln().toInt()) {
+        println(validityCheckBracket(readln()))
+    }
+}
+
+fun validityCheckBracket(testCase: String): String {
+    val bracketStack = ArrayList<Char>()
+    testCase.forEach {
+        when (it) {
+            '(' -> bracketStack.add(it)
+            ')' -> {
+                if (bracketStack.isEmpty()) return "NO"
+                bracketStack.removeLastOrNull()
+            }
+        }
+    }
+    if (bracketStack.isEmpty())
+        return "YES"
+    return "NO"
+}
