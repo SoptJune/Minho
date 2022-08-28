32bb49c3a9fd657df39fe4e6b76f0a9f0f4d3e3d
minho <my_alsgh@naver.com>
2022-08-28 21:16:34 +0900

    [Feat] Restore Problem Complete

---
+++ src/main/kotlin/implement/Restore.kt
@@ -0,0 +1,44 @@
+package implement
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+
+val array = ArrayList<String>()
+
+fun main() {
+    repeat(readln().toInt()) {
+        array.add(readln())
+    }
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var i = array.size - 1
+    while (i >= 0) {
+        val (command, optional, currentTime) = array[i].split(" ")
+        if (command == "undo") {
+            array.removeAt(i--)
+            val untilTime = (currentTime.toInt() - optional.toInt())
+            if (i >= 0) {
+                while (true) {
+                    if (i < 0) {
+                        break
+                    }
+                    val (subCommand, subOptional, subCurrentTime) = array[i].split(" ")
+                    val subTime = subCurrentTime.toInt()
+                    if (untilTime <= subTime) {
+                        array.removeAt(i--)
+                    } else {
+                        break
+                    }
+                }
+            }
+        }else{
+            i--
+        }
+    }
+    val str = StringBuilder()
+    array.forEach {
+        str.append(it[5])
+    }
+    bw.write(str.toString())
+    bw.close()
+}
