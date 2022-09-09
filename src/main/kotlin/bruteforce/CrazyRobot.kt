5c02101d8157a7eed96707ca7bdc3b2b21382376
minho <my_alsgh@naver.com>
2022-09-09 19:12:40 +0900

    [Feat] CrazyRobot Problem Complete

---
+++ src/main/kotlin/bruteforce/CrazyRobot.kt
@@ -0,0 +1,35 @@
+package bruteforce
+
+
+val intRangeX = intArrayOf(0, 0, 1, -1)
+val intRangeY = intArrayOf(1, -1, 0, 0)
+val fourWayDirection = DoubleArray(4)
+var N = 0
+var totalProbability = 0.0
+
+fun main() {
+    var visitMap = Array(30) { BooleanArray(30) }
+    visitMap[15][15] = true
+    readln().split(" ")
+        .mapIndexed { index, s -> if (index == 0) N = s.toInt() else fourWayDirection[index - 1] = s.toDouble() * 0.01 }
+    DFS(0, 15, 15, visitMap, 1.0)
+    println(totalProbability)
+}
+
+fun DFS(cnt: Int, currentX: Int, currentY: Int, visited: Array<BooleanArray>, cumulativeResult: Double) {
+    if (cnt == N) {
+        totalProbability += cumulativeResult
+        return
+    }
+    for (i in 0 until 4) {
+        if (fourWayDirection[i] == 0.0) continue
+        val dx = currentX + intRangeX[i]
+        val dy = currentY + intRangeY[i]
+        if (dx < 1 || dy < 1 || dx > 29 || dy > 29) continue
+        if (!visited[dx][dy]) {
+            visited[dx][dy] = true
+            DFS(cnt + 1, dx, dy, visited, cumulativeResult * fourWayDirection[i])
+            visited[dx][dy] = false
+        }
+    }
+}
