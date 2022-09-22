19501902930d23fae2163d2bf9d7c968af97259e
minho <my_alsgh@naver.com>
2022-09-22 20:52:03 +0900

    [Feat] String Explosion Problem Complete

---
+++ src/main/kotlin/string/StringExplosion.kt
@@ -0,0 +1,36 @@
+package string
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.Stack
+
+fun main() {
+    var str = readln()
+    val bomb = readln()
+    val stack = Stack<Char>()
+    val answer = StringBuilder()
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    str.forEach {
+        stack.push(it)
+        if (stack.size >= bomb.length && stack.peek() == bomb.last()) {
+            var bool = true
+            bomb.forEachIndexed { index, c ->
+                if (stack[stack.size - bomb.length + index] != c) {
+                    bool = false
+                    return@forEachIndexed
+                }
+            }
+            if (bool) {
+                repeat(bomb.length) {
+                    stack.pop()
+                }
+            }
+        }
+    }
+    stack.forEach {
+        answer.append(it)
+    }
+    if (answer.isNotEmpty()) bw.write(answer.toString()) else bw.write("FRULA")
+    bw.close()
+}
