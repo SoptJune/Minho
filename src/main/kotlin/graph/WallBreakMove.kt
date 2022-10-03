06f47503fd407f6caba9236cac07e02ee2a62005
minho <my_alsgh@naver.com>
2022-10-03 22:56:25 +0900

    [Feat] WallBreakAndMove Problem Complete

--- src/main/kotlin/graph/WallBreakMove.kt
+++ src/main/kotlin/graph/WallBreakMove.kt
@@ -2,76 +2,87 @@ package graph
 
 import java.util.LinkedList
 import java.util.Queue
-import java.util.Stack
+import kotlin.math.max
 import kotlin.math.min
 
-/*var answer: Array<IntArray>*/
-val queue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
-var minCount = 100000
-val intRangeX = intArrayOf(0, 0, 1, -1)
-val intRangeY = intArrayOf(1, -1, 0, 0)
-var miro = ArrayList<MutableList<Char>>()
-var endM: Int = 0
-var endN: Int = 0
-lateinit var tour: Array<BooleanArray>
-lateinit var answer: Array<IntArray>
+
+lateinit var answer: Array<Array<IntArray>>
+lateinit var tour: Array<Array<BooleanArray>>
+var endN = 0
+var endM = 0
 fun main() {
-    val (N, M) = kotlin.io.readln().split(' ').map { it.toInt() }
+    val (N, M) = readln().split(' ').map { it.toInt() }
     endN = N
     endM = M
-    tour = Array(N) { BooleanArray(M) }
+    val queue: Queue<Triple<Int, Int, Boolean>> = LinkedList<Triple<Int, Int, Boolean>>()
+    val intRangeX = intArrayOf(0, 0, 1, -1)
+    val intRangeY = intArrayOf(1, -1, 0, 0)
+    var miro = ArrayList<MutableList<Char>>()
+    tour = Array(N) { Array(M) { BooleanArray(2) } }
     repeat(N) {
         miro.addAll(listOf(readln().toMutableList()))
     }
-    answer = Array(N) { IntArray(M) }
-    answer[0][0] = 1
-    tour[0][0] = true
-    queue.offer(Pair(0,0))
-/*
 
-    answer[N - 1][M - 1] = -1
-    answer[0][0] = 1
-*/
-    searchBFSGraph()
-    print(minCount)
-
-/*
-    queue.offer(Pair(0, 0))
+    answer = Array(N) { Array(M) { IntArray(2) } }
+    answer[N - 1][M - 1][0] = -1
+    answer[N - 1][M - 1][1] = -1
+    answer[0][0][0] = 1
+    queue.offer(Triple(0, 0, false))
     while (queue.isNotEmpty()) {
         val data = queue.poll()
-        for (i in 0 until 4) {
-            val dx = data.second + intRangeX[i]
-            val dy = data.first + intRangeY[i]
+        val visit = data.third
 
-            if (dx < 0 || dy < 0 || dx >= M || dy >= N) continue
-            if (dx == M && dy == N) {
-                answer[dy][dx] =
-                    if (answer[dy][dx] == -1) answer[data.first][data.second] + 1
-                    else min(answer[data.first][data.second] + 1, answer[dy][dx])
+        if (visit) {
+            for (i in 0 until 4) {
+                val dx = data.second + intRangeX[i]
+                val dy = data.first + intRangeY[i]
+                if (dx < 0 || dy < 0 || dx >= M || dy >= N) continue
+                if (dx == M - 1 && dy == N - 1) {
+                    insertMinValue(1, data)
+                    continue
+                }
+                if (!tour[dy][dx][1] && miro[dy][dx] == '0') {
+                    queue.add(Triple(dy, dx, true))
+                    answer[dy][dx][1] = answer[data.first][data.second][1] + 1
+                    tour[dy][dx][1] = true
+                }
+                //tour[dy][dx] = true
             }
-            if (!tour[dy][dx]) {
-                if (miro[data.first][data.second].second) {
-                    if (miro[dy][dx].first == '0') {
-                        tour[dy][dx] = true
-                        miro[dy][dx] = Pair(miro[dy][dx].first, true)
-                        answer[dy][dx] = answer[data.first][data.second] + 1
-                        queue.add(Pair(dy, dx))
-                    } else continue
-                } else if (miro[dy][dx].first == '1' && !miro[dy][dx].second) {
-                    tour[dy][dx] = true
-                    miro[dy][dx] = Pair(miro[dy][dx].first, true)
-                    answer[dy][dx] = answer[data.first][data.second] + 1
-                    queue.add(Pair(dy, dx))
-                } else if (miro[dy][dx].first == '0') {
-                    tour[dy][dx] = true
-                    answer[dy][dx] = answer[data.first][data.second] + 1
-                    queue.add(Pair(dy, dx))
+        } else {
+            for (i in 0 until 4) {
+                val dx = data.second + intRangeX[i]
+                val dy = data.first + intRangeY[i]
+                if (dx < 0 || dy < 0 || dx >= M || dy >= N) continue
+                if (dx == M - 1 && dy == N - 1) {
+                    insertMinValue(0, data)
+                    continue
+                }
+                if (!tour[dy][dx][0]) {
+                    if (miro[dy][dx] == '0') {
+                        queue.add(Triple(dy, dx, false))
+                        answer[dy][dx][0] = answer[data.first][data.second][0] + 1
+                        tour[dy][dx][0] = true
+                    } else {
+                        queue.add(Triple(dy, dx, true))
+                        answer[dy][dx][1] = answer[data.first][data.second][0] + 1
+                        tour[dy][dx][1] = true
+                    }
                 }
             }
         }
     }
+    if (answer[N - 1][M - 1][0] == -1 || answer[N - 1][M - 1][1] == -1) {
+        print(max(answer[N - 1][M - 1][0], answer[N - 1][M - 1][1]))
+    } else {
+        print(min(answer[N - 1][M - 1][0], answer[N - 1][M - 1][1]))
+    }
+}
 
-    print(answer[N - 1][M - 1])*/
+fun insertMinValue(isWallBroken: Int, data: Triple<Int, Int, Boolean>) {
+    answer[endN - 1][endM - 1][isWallBroken] =
+        if (answer[endN - 1][endM - 1][isWallBroken] == -1) answer[data.first][data.second][isWallBroken] + 1
+        else min(answer[data.first][data.second][isWallBroken] + 1, answer[endN - 1][endM - 1][isWallBroken])
+    tour[endN - 1][endM - 1][isWallBroken] = true
 }
 //DFS
 /*for (i in 0 until N) {
@@ -87,9 +98,9 @@ fun main() {
 }
 print(maxCount)
 }*/
+/*
 
-
-fun searchBFSGraph() {
+fun searchBFSGraph(): Int {
     while (queue.isNotEmpty()) {
         val data = queue.poll()
         for (i in 0 until 4) {
@@ -99,15 +110,23 @@ fun searchBFSGraph() {
             if (dx < 0 || dy < 0 || dx >= endM || dy >= endN) continue
             if (!tour[dy][dx] && miro[dy][dx] != '0') {
                 tour[dy][dx] = true
-                answer[dy][dx] = answer[data.first][data.second] + 1
-                if (dx == endM - 1 && dy == endN-1) {
-                    minCount = min(minCount, answer[dy][dx])
+                if(dx==endM-1 && dy == endN){
+
+                if (minCount == -1) {
+                    answer[dy][dx] = answer[data.first][data.second] + 1
+                    minCount = answer[dy][dx]
+                }else{
+
                 }
+
+                }
+                answer[dy][dx] = answer[data.first][data.second] + 1
                 queue.add(Pair(dy, dx))
             }
         }
     }
-}
+    return answer[endN - 1][endM - 1]
+}*/
 
 /*
 fun searchDFSGraph(count: Int, currentX: Int, currentY: Int) {
