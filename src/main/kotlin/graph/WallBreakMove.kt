81375291d9d0e8788c52d4773594ac4c885e6f8b
minho <my_alsgh@naver.com>
2022-10-03 12:47:54 +0900

    [Chore] WallBreakMove BFS Time Problem

--- src/main/kotlin/graph/WallBreakMove.kt
+++ src/main/kotlin/graph/WallBreakMove.kt
@@ -1,50 +1,116 @@
 package graph
 
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Stack
 import kotlin.math.min
 
-var endN = 0
-var endM = 0
-lateinit var tour: Array<BooleanArray>
-lateinit var miro: ArrayList<MutableList<Char>>
-lateinit var answer: Array<IntArray>
 
-val intRangeX = intArrayOf(0, 0, 1, -1)
-val intRangeY = intArrayOf(1, -1, 0, 0)
-
-var maxCount = -1
 fun main() {
+
+    var answer: Array<IntArray>
+    val queue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
+    var minCount = -1
+    val intRangeX = intArrayOf(0, 0, 1, -1)
+    val intRangeY = intArrayOf(1, -1, 0, 0)
     val (N, M) = readln().split(' ').map { it.toInt() }
-    endN = N
-    endM = M
-    tour = Array(N) { BooleanArray(M) }
-    miro = ArrayList<MutableList<Char>>()
+    val tour = Array(N) { BooleanArray(M) }
+    var miro = ArrayList<MutableList<Pair<Char, Boolean>>>()
+    repeat(N) {
+        miro.addAll(listOf(readln().map {
+            Pair(it, false)
+        }.toMutableList()))
+    }
     answer = Array(N) { IntArray(M) }
+
     answer[N - 1][M - 1] = -1
     answer[0][0] = 1
-    answer.forEach {
-        println(it.toList())
-    }
-    repeat(N) {
-        miro.addAll(listOf(readln().toMutableList()))
-    }
-    for (i in 0 until N) {
-        for (j in 0 until M) {
-            if (miro[i][j] == '1') {
-                miro[i][j] = '0'
-                searchDFSGraph(1, 0, 0)
-                miro[i][j] = '1'
-                tour = Array(N) { BooleanArray(M) }
+
+    queue.offer(Pair(0, 0))
+    while (queue.isNotEmpty()) {
+        val data = queue.poll()
+        for (i in 0 until 4) {
+            val dx = data.second + intRangeX[i]
+            val dy = data.first + intRangeY[i]
+
+            if (dx < 0 || dy < 0 || dx >= M || dy >= N) continue
+            if (dx == M && dy == N) {
+                answer[dy][dx] =
+                    if (answer[dy][dx] == -1) answer[data.first][data.second] + 1
+                    else min(answer[data.first][data.second] + 1, answer[dy][dx])
+            }
+            if (!tour[dy][dx]) {
+                if (miro[data.first][data.second].second) {
+                    if (miro[dy][dx].first == '0') {
+                        tour[dy][dx] = true
+                        miro[dy][dx] = Pair(miro[dy][dx].first, true)
+                        answer[dy][dx] = answer[data.first][data.second] + 1
+                        queue.add(Pair(dy, dx))
+                    } else continue
+                } else if (miro[dy][dx].first == '1' && !miro[dy][dx].second) {
+                    tour[dy][dx] = true
+                    miro[dy][dx] = Pair(miro[dy][dx].first, true)
+                    answer[dy][dx] = answer[data.first][data.second] + 1
+                    queue.add(Pair(dy, dx))
+                } else if (miro[dy][dx].first == '0') {
+                    tour[dy][dx] = true
+                    answer[dy][dx] = answer[data.first][data.second] + 1
+                    queue.add(Pair(dy, dx))
+                }
             }
         }
     }
-    print(maxCount)
-}
 
+    print(answer[N - 1][M - 1])
+}
+//DFS
+/*for (i in 0 until N) {
+    for (j in 0 until M) {
+        if (miro[i][j] == '1') {
+            miro[i][j] = '0'
+            tour[i][j]=true
+            searchBFSGraph(1, 0, 0)
+            miro[i][j] = '1'
+            tour = Array(N) { BooleanArray(M) }
+        }
+    }
+}
+print(maxCount)
+}*/
+
+/*
+
+fun searchBFSGraph(): Int {
+
+
+    while (stack.isNotEmpty()) {
+        val data = stack.pop()
+        for (i in 0 until 4) {
+            val dx = data.second + intRangeX[i]
+            val dy = data.first + intRangeY[i]
+
+            if (dx < 0 || dy < 0 || dx >= endM || dy >= endN) continue
+            if (!tour[dy][dx] && miro[dy][dx] != '1') {
+                tour[dy][dx] = true
+                answer[dy][dx] = answer[data.first][data.second] + 1
+                stack.push(Pair(dy, dx))
+            }
+        }
+    }
+    answer.forEach {
+        println(it.toList())
+    }
+    return answer[endN - 1][endM - 1]
+}
+*/
+
+/*
 fun searchDFSGraph(count: Int, currentX: Int, currentY: Int) {
     if (currentX == endM - 1 && currentY == endN - 1) {
-        maxCount = if (maxCount == -1) count
-        else min(count, maxCount)
+        minCount = if (minCount == -1) count
+        else min(count, minCount)
     }
+
     for (i in 0 until 4) {
         val dx = currentX + intRangeX[i]
         val dy = currentY + intRangeY[i]
@@ -56,4 +122,4 @@ fun searchDFSGraph(count: Int, currentX: Int, currentY: Int) {
             tour[dy][dx] = false
         }
     }
-}
+}*/
