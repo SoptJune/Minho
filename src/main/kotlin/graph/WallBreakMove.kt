e6d4d930606b96b9cdd6f6d261b0bb59f4a579dc
minho <my_alsgh@naver.com>
2022-10-03 17:24:00 +0900

    [Feat] 2178 Miro BFS Complete

--- src/main/kotlin/graph/WallBreakMove.kt
+++ src/main/kotlin/graph/WallBreakMove.kt
@@ -5,27 +5,37 @@ import java.util.Queue
 import java.util.Stack
 import kotlin.math.min
 
-
+/*var answer: Array<IntArray>*/
+val queue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
+var minCount = 100000
+val intRangeX = intArrayOf(0, 0, 1, -1)
+val intRangeY = intArrayOf(1, -1, 0, 0)
+var miro = ArrayList<MutableList<Char>>()
+var endM: Int = 0
+var endN: Int = 0
+lateinit var tour: Array<BooleanArray>
+lateinit var answer: Array<IntArray>
 fun main() {
-
-    var answer: Array<IntArray>
-    val queue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
-    var minCount = -1
-    val intRangeX = intArrayOf(0, 0, 1, -1)
-    val intRangeY = intArrayOf(1, -1, 0, 0)
-    val (N, M) = readln().split(' ').map { it.toInt() }
-    val tour = Array(N) { BooleanArray(M) }
-    var miro = ArrayList<MutableList<Pair<Char, Boolean>>>()
+    val (N, M) = kotlin.io.readln().split(' ').map { it.toInt() }
+    endN = N
+    endM = M
+    tour = Array(N) { BooleanArray(M) }
     repeat(N) {
-        miro.addAll(listOf(readln().map {
-            Pair(it, false)
-        }.toMutableList()))
+        miro.addAll(listOf(readln().toMutableList()))
     }
     answer = Array(N) { IntArray(M) }
+    answer[0][0] = 1
+    tour[0][0] = true
+    queue.offer(Pair(0,0))
+/*
 
     answer[N - 1][M - 1] = -1
     answer[0][0] = 1
+*/
+    searchBFSGraph()
+    print(minCount)
 
+/*
     queue.offer(Pair(0, 0))
     while (queue.isNotEmpty()) {
         val data = queue.poll()
@@ -61,7 +71,7 @@ fun main() {
         }
     }
 
-    print(answer[N - 1][M - 1])
+    print(answer[N - 1][M - 1])*/
 }
 //DFS
 /*for (i in 0 until N) {
@@ -78,31 +88,26 @@ fun main() {
 print(maxCount)
 }*/
 
-/*
 
-fun searchBFSGraph(): Int {
-
-
-    while (stack.isNotEmpty()) {
-        val data = stack.pop()
+fun searchBFSGraph() {
+    while (queue.isNotEmpty()) {
+        val data = queue.poll()
         for (i in 0 until 4) {
             val dx = data.second + intRangeX[i]
             val dy = data.first + intRangeY[i]
 
             if (dx < 0 || dy < 0 || dx >= endM || dy >= endN) continue
-            if (!tour[dy][dx] && miro[dy][dx] != '1') {
+            if (!tour[dy][dx] && miro[dy][dx] != '0') {
                 tour[dy][dx] = true
                 answer[dy][dx] = answer[data.first][data.second] + 1
-                stack.push(Pair(dy, dx))
+                if (dx == endM - 1 && dy == endN-1) {
+                    minCount = min(minCount, answer[dy][dx])
+                }
+                queue.add(Pair(dy, dx))
             }
         }
     }
-    answer.forEach {
-        println(it.toList())
-    }
-    return answer[endN - 1][endM - 1]
 }
-*/
 
 /*
 fun searchDFSGraph(count: Int, currentX: Int, currentY: Int) {
