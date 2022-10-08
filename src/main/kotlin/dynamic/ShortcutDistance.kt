061005672816888b059ccfd8c40a22a43a482274
minho <my_alsgh@naver.com>
2022-10-09 02:02:19 +0900

    [Feat] ShortCut Problem Complete

--- src/main/kotlin/dynamic/ShortcutDistance.kt
+++ src/main/kotlin/dynamic/ShortcutDistance.kt
@@ -11,25 +11,25 @@ fun main() {
     val (tempN, tempD) = readln().split(" ").map { it.toInt() }
     N = tempN
     D = tempD
-    if (N == 0) {
+    if (N == 0) { //지름길 없으면 냅다 종료
         print(D)
         return
     }
     visit = IntArray(D + 1) { it }
     repeat(N) {
         readln().split(" ").map { it.toInt() }.let {
-            if (it[1] <= D && it[2] <= D && it[1] - it[0] >= it[2])
+            if (it[1] <= D && it[1] - it[0] > it[2]) //도착지가 D보다 작으면서, 도착 위치 - 시작 위치가 길이보다 클 때만 add
                 array.add(Triple(it[0], it[1], it[2]))
         }
     }
 
     val comparator: Comparator<Triple<Int, Int, Int>> =
         compareBy<Triple<Int, Int, Int>> { it.first }.thenBy { it.second }.thenByDescending { it.third }
-
+    //시작 위치순 -> 도착 위치 역순 -> 거리 역순 기준으로 정렬
     array.sortWith(comparator)
     for (i in visit.indices) {
-        if(i>0) visit[i] = min(visit[i], visit[i - 1] + 1)
-        array.filter { it.first == i && (visit[i] + it.third) < visit[it.second] }.forEach {
+        if (i > 0) visit[i] = min(visit[i], visit[i - 1] + 1)
+        array.filter { it.first == i && (visit[i] + it.third) < visit[it.second] }.forEach {//시작 위치이면서, 지름길 썼을 때 이득인 경우의 리스트만 뽑아냄
             visit[it.second] = visit[it.first] + it.third
         }
     }
