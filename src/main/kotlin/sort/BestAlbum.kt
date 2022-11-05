d9741207256b2eb01e016afbcecbb4999c0270c5
minho <my_alsgh@naver.com>
2022-11-05 19:04:22 +0900

    [Feat] Best Album Problem Complete

---
+++ src/main/kotlin/sort/BestAlbum.kt
@@ -0,0 +1,26 @@
+package sort
+
+fun main() {
+    solution(arrayOf("classic", "pop", "classic", "classic", "pop"), intArrayOf(500, 600, 150, 800, 2500))
+}
+
+fun solution(genres: Array<String>, plays: IntArray): IntArray {
+    val pairAlbum = ArrayList<Triple<String, Int, Int>>()
+    for (i in genres.indices) {
+        pairAlbum.add(Triple(genres[i], plays[i], i))
+    }
+    val newGroup = pairAlbum.groupBy { it.first }.map {//장르 기준으로 그룹화, 재생수 accumulate 구하는 코드, 마지막 정렬까지
+        it.key to it.value.sumOf { pair -> pair.second }
+    }.sortedByDescending { it.second }.map { it.first }
+
+    pairAlbum.sortedWith(
+        compareBy<Triple<String, Int, Int>> { it.first }
+            .thenByDescending { it.second }.thenBy { it.third }).toList()
+
+    var answer = mutableListOf<Int>()
+    newGroup.forEach { genres ->
+        answer.addAll(pairAlbum.filter { it.first == genres }.map { it.third }.take(2))
+    }
+    println(answer)
+    return answer.toIntArray()
+}
