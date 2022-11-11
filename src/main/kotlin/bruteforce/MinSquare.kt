8a534a8f3fa0284edfac96e64cb6391d1b569c6e
minho <my_alsgh@naver.com>
2022-11-12 03:20:15 +0900

    [Feat] MinSquare Problem Complete

---
+++ src/main/kotlin/bruteforce/MinSquare.kt
@@ -0,0 +1,26 @@
+package bruteforce
+
+import java.lang.Integer.max
+
+var maxOfMax = 0
+var minOfMax = 0
+
+fun main() {
+    println(solution(arrayOf(intArrayOf(14, 4), intArrayOf(19, 6), intArrayOf(6, 16), intArrayOf(18, 7), intArrayOf(7, 11))))
+}
+
+fun solution(sizes: Array<IntArray>): Int {
+    sizes.map { swapSide(it) }
+    return maxOfMax * minOfMax
+}
+
+fun swapSide(pair: IntArray) {
+    val (first, second) = pair
+    return if (first > second) {
+        minOfMax = max(second, minOfMax)
+        maxOfMax = max(first, maxOfMax)
+    } else {
+        minOfMax = max(first, minOfMax)
+        maxOfMax = max(second, maxOfMax)
+    }
+}
