b6ab94b4f00e83f4a87aa1190707582aa00acd45
minho <my_alsgh@naver.com>
2022-11-22 21:40:22 +0900

    [Feat] Best Number Problem Complete

---
+++ src/main/kotlin/sort/BestNumber.kt
@@ -0,0 +1,37 @@
+package sort
+
+fun main() {
+    println(solution(intArrayOf(3, 30, 34, 5, 9)))
+}
+
+fun solution(numbers: IntArray): String {
+    val comparator = Comparator<String> { o1, o2 ->
+        (o2 + o1).compareTo(o1 + o2)
+    }
+    if(numbers.all { it ==0 }) return "0"
+    return numbers.map { it.toString() }.sortedWith(comparator).joinToString("")
+}
+
+
+/* 3 34 4 4954
+33 318
+3
+
+34
+349
+343
+340
+
+34349
+34934
+
+34343
+34334
+
+309
+
+342
+33
+
+
+ */
