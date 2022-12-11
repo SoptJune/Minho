f8c7c1081fbab7f431e7d5395db9100eea50d555
minho <my_alsgh@naver.com>
2022-12-11 21:22:22 +0900

    [Feat] StairUp Problem Complete

---
+++ src/main/kotlin/dynamic/StairUp.kt
@@ -0,0 +1,29 @@
+package dynamic
+
+import kotlin.math.max
+
+fun main() {
+    val stair = mutableListOf<Int>()
+    val N = readln().toInt()
+    repeat(N) {
+        stair.add(readln().toInt())
+    }
+    if (N <= 2) {
+        println(stair.sum())
+        return
+    }
+    val weight = IntArray(N) { 0 }
+    weight[0] = stair[0]
+    weight[1] = stair[0] + stair[1]
+    weight[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+    for (i in 3 until N) {
+        weight[i] = max(weight[i - 2] + stair[i], stair[i - 1] + stair[i] + weight[i - 3])
+    }
+    println(weight[N-1])
+}
+/*
+첫번째 밟은 경우
+첫번째 두번째 밟은 경우
+Max(첫번째 세번째 밟은 경우, 두번째 세번째 밟은 경우)
+
+ */
